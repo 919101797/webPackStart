410b1cfbee56283102303bacec1d990040bde9f4
chao <chenchao@croot.com>
2022-03-24 19:34:08 +0800

    fix:

---
+++ src/views/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: cc
+ * @Date: 2022-03-24 19:33:37
+ * @LastEditors: cc
+ * @LastEditTime: 2022-03-24 19:33:44
+ * @important: 重要提醒
+ * @Description: 备注内容
+ * @FilePath: \webpack\src\views\index.js
+ */
