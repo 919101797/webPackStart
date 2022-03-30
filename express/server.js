958ef909abdcae0de22f17cc83304c9c5bb9bd1b
chao <chenchao@croot.com>
2022-03-30 19:30:26 +0800

    fix

--- express/server.js
+++ express/server.js
@@ -2,7 +2,7 @@
  * @Author: cc
  * @Date: 2022-03-30 18:06:53
  * @LastEditors: cc
- * @LastEditTime: 2022-03-30 18:53:59
+ * @LastEditTime: 2022-03-30 19:10:49
  * @important: 重要提醒
  * @Description: 备注内容
  * @FilePath: \webpack\express\server.js
@@ -18,12 +18,12 @@ const compiler = webpack(config);
 // 告知 express 使用 webpack-dev-middleware，
 // 以及将 webpack.config.js 配置文件作为基础配置。
 app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
+    webpackDevMiddleware(compiler, {
+      publicPath: config.output.publicPath,
+    })
+  );
 
 // 将文件 serve 到 port 3000。
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!\n');
+  console.log('开启服务,端口：3000\n');
 });
